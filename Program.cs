fe83660e4047825d9c0b73e8a8c358308fbfd8c6
Marcus Ruohonen <marcus.ruohonen@gmail.com>
2024-02-06 13:18:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Hooman
+    {
+        // Define properties of Hooman
+        string name;
+        int age;
+        string gender;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
