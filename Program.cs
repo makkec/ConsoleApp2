d770808c48d4529ba424fc928747d6db23438093
Marcus Ruohonen <marcus.ruohonen@gmail.com>
2024-02-06 14:28:16 +0200

    tehtävyksiä

--- Program.cs
+++ Program.cs
@@ -8,10 +8,38 @@ namespace ConsoleApp2
 {
     class Hooman
     {
-        // Define properties of Hooman
-        string name;
-        int age;
-        string gender;
+        // Define properties of Hooman ie. fields
+        string name = "Essi Esimerkki";
+        int age = 30;
+        string gender = "Emäntä";
+
+        Hooman()
+        {
+
+        }
+
+        // Constructor with one argument
+
+        Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with 2 arguments
+
+        Hooman(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Constructor with 3 arguments
+        Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
     }
     internal class Program
     {
