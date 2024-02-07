ec911ca2dbc716575c0db1301eb2b92b0f6ded30
Marcus Ruohonen <marcus.ruohonen@gmail.com>
2024-02-07 10:57:59 +0200

    moi

--- Program.cs
+++ Program.cs
@@ -5,46 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp2
+
 {
     class Hooman
     {
         // Define properties of Hooman ie. fields
-        string name = "Essi Esimerkki";
-        int age = 30;
-        string gender = "Emäntä";
+        public string name = "Essi Esimerkki";
+        public int age = 30;
+        public string gender = "Emäntä";
 
-        Hooman()
+        // Default constructor w/o arguments
+        // No need to define, will be created automatically
+        public Hooman()
         {
 
         }
 
         // Constructor with one argument
-
-        Hooman(string name)
+        public Hooman(string name)
         {
             this.name = name;
         }
 
-        // Constructor with 2 arguments
-
-        Hooman(string name, int age)
+        // Constructor with two arguments
+        public Hooman(string name, int age)
         {
             this.name = name;
             this.age = age;
         }
 
-        // Constructor with 3 arguments
-        Hooman(string name, int age, string gender)
+        // Constructor with three arguments
+        public Hooman(string name, int age, string gender)
         {
             this.name = name;
             this.age = age;
             this.gender = gender;
+
+        }
+
+        // A method to say something
+        public void SayOpinion()
+        {
+            Console.WriteLine("Voi, lemmikit ne on elämän suola");
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Create (instantiate) a hooman object from Hooman class
+            Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
+
+            // Call the SayOpinion method
+            owner.SayOpinion();
+
+            // Keep the window open until key is pressed
+
+            string who = owner.name;
+
+            Console.WriteLine("totesi " + who);
+
+            Console.ReadLine();
         }
     }
 }
